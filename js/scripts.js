e54cff5bb65c290a8b9bad55dc9d53a95d70c61a
hrn21 <p16201976@my365.dmu.ac.uk>
2018-10-29 12:39:53 +0000

    lab4

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
 console.log("hello");
+
+const menuToggle= document.getElementById("menuToggle");
+
+const toggleEs6= toggleMenu=> console.log("called toggleMenu");
+menuToggle.addEventListener('click', toggleEs6);
+
+const menuNav= document.getElementById(menuNav);
